e73c4c5c854b4a38d21c859684ff73b5d15db2d9
Dan Rosen <drosen@precipart.com>
2020-06-07 09:37:08 -0400

    dynamic colors and emojis

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -2,6 +2,41 @@ import React, { Component } from 'react'
 import styles from './Joke.module.css'
 
 class Joke extends Component {
+    getColor() {
+        if (this.props.votes >= 15) {
+            return '#4CAF50'
+        } else if (this.props.votes >= 12) {
+            return '#8BC34A'
+        } else if (this.props.votes >= 9) {
+            return '#CDDC39'
+        } else if (this.props.votes >= 6) {
+            return '#FFEB3B'
+        } else if (this.props.votes >= 3) {
+            return '#FFC107'
+        } else if (this.props.votes >= 0) {
+            return '#FF9800'
+        } else {
+            return '#f44336'
+        }
+    }
+    getEmoji() {
+        if (this.props.votes >= 15) {
+            return 'em em-rolling_on_the_floor_laughing'
+        } else if (this.props.votes >= 12) {
+            return 'em em-laughing'
+        } else if (this.props.votes >= 9) {
+            return 'em em-smiley'
+        } else if (this.props.votes >= 6) {
+            return 'em em-slightly_smiling_face'
+        } else if (this.props.votes >= 3) {
+            return 'em em-neutral_face'
+        } else if (this.props.votes >= 0) {
+            return 'em em-confused'
+        } else {
+            return 'em em-angry'
+        }
+    }
+
     render() {
         return (
             <div className={styles.Joke}>
@@ -10,7 +45,12 @@ class Joke extends Component {
                         className={`fas fa-arrow-up ${styles.JokeVoteArrow}`}
                         onClick={this.props.upvote}
                     ></i>
-                    <span className={styles.JokeVotes}>{this.props.votes}</span>
+                    <span
+                        className={styles.JokeVotes}
+                        style={{ borderColor: this.getColor() }}
+                    >
+                        {this.props.votes}
+                    </span>
                     <i
                         className={`fas fa-arrow-down ${styles.JokeVoteArrow}`}
                         onClick={this.props.downvote}
@@ -18,10 +58,7 @@ class Joke extends Component {
                 </div>
                 <div className={styles.JokeText}>{this.props.text}</div>
                 <div className={styles.JokeSmiley}>
-                    <i
-                        class='em em-rolling_on_the_floor_laughing'
-                        aria-label='ROLLING ON THE FLOOR LAUGHING'
-                    ></i>
+                    <i className={this.getEmoji()}></i>
                 </div>
             </div>
         )
