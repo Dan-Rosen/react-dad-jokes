af1beea3d238657b00c2fdcddb0419033c5c8648
Dan Rosen <drosen@precipart.com>
2020-06-07 10:17:21 -0400

    prevent duplicates, bug fixes, error trapping

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -16,6 +16,7 @@ class JokeList extends Component {
             jokes: JSON.parse(window.localStorage.getItem('jokes') || '[]'),
             loading: false
         }
+        this.seenJokes = new Set(this.state.jokes.map((j) => j.text))
         this.handleClick = this.handleClick.bind(this)
     }
 
@@ -24,24 +25,32 @@ class JokeList extends Component {
     }
 
     async getJokes() {
-        let jokes = []
-        while (jokes.length < this.props.numJokesToGet) {
-            let res = axios.get('https://icanhazdadjoke.com/', {
-                headers: { accept: 'application/json' }
-            })
-            jokes.push({ id: uuid(), text: (await res).data.joke, votes: 0 })
+        try {
+            let jokes = []
+            while (jokes.length < this.props.numJokesToGet) {
+                let res = await axios.get('https://icanhazdadjoke.com/', {
+                    headers: { accept: 'application/json' }
+                })
+                let newJoke = res.data.joke
+                if (!this.seenJokes.has(newJoke)) {
+                    jokes.push({ id: uuid(), text: newJoke, votes: 0 })
+                }
+            }
+            this.setState(
+                (st) => ({
+                    jokes: [...st.jokes, ...jokes],
+                    loading: false
+                }),
+                () =>
+                    window.localStorage.setItem(
+                        'jokes',
+                        JSON.stringify(this.state.jokes)
+                    )
+            )
+        } catch (err) {
+            alert(err)
+            this.setState({ loading: false })
         }
-        this.setState(
-            (st) => ({
-                jokes: [...st.jokes, ...jokes],
-                loading: false
-            }),
-            () =>
-                window.localStorage.setItem(
-                    'jokes',
-                    JSON.stringify(this.state.jokes)
-                )
-        )
     }
 
     handleVote(id, delta) {
