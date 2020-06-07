9a4728d07d25ca9f6bb87cac839d865ea17a80c8
Dan Rosen <drosen@precipart.com>
2020-06-07 09:32:21 -0400

    styling joke component

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -7,16 +7,22 @@ class Joke extends Component {
             <div className={styles.Joke}>
                 <div className={styles.JokeButtons}>
                     <i
-                        className='fas fa-arrow-up'
+                        className={`fas fa-arrow-up ${styles.JokeVoteArrow}`}
                         onClick={this.props.upvote}
                     ></i>
-                    <span>{this.props.votes}</span>
+                    <span className={styles.JokeVotes}>{this.props.votes}</span>
                     <i
-                        className='fas fa-arrow-down'
+                        className={`fas fa-arrow-down ${styles.JokeVoteArrow}`}
                         onClick={this.props.downvote}
                     ></i>
                 </div>
                 <div className={styles.JokeText}>{this.props.text}</div>
+                <div className={styles.JokeSmiley}>
+                    <i
+                        class='em em-rolling_on_the_floor_laughing'
+                        aria-label='ROLLING ON THE FLOOR LAUGHING'
+                    ></i>
+                </div>
             </div>
         )
     }
